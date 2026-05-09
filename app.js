0277bc8b6bedd695b918b6270a1e111b93c6cff4
ahmed <ahmeduni.126@gmail.com>
2026-05-09 10:28:58 +0300

    hesap makinesi tamamlandi

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
-console.log("Proje başarıyla yüklendi!");
+function appendToDisplay(value) {
+  const display = document.getElementById('display');
+  if (display.innerText === '0') {
+    display.innerText = value;
+  } else {
+    display.innerText += value;
+  }
+}
+
+function clearDisplay() {
+  document.getElementById('display').innerText = '0';
+}
+
+function deleteLast() {
+  const display = document.getElementById('display');
+  if (display.innerText.length === 1) {
+    display.innerText = '0';
+  } else {
+    display.innerText = display.innerText.slice(0, -1);
+  }
+}
+
+function calculate() {
+  const display = document.getElementById('display');
+  try {
+    display.innerText = eval(display.innerText);
+  } catch {
+    display.innerText = 'Hata';
+  }
+}
  
